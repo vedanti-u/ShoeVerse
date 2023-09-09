d386d83846c0b38687574ab5b02e3567a9ed8eb3
Vedanti.U <vedanti.vvu@gmail.com>
2023-09-09 16:15:23 +0530

    correct routes structure

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const mainjs = require("./main.js")
+const mainjs = require("./routes/main.js")
 
-app.use("/main",mainjs);
+var bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.send('Hello world :) <3')
 })
+
+app.use("/main",mainjs);
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`)
--- routes/main.js
+++ routes/main.js
@@ -2,17 +2,18 @@ var express = require('express');
 var app = express();
 
 var router = express.Router();
-const sql = require("./server.js");
-//console.log(sql);
+const sql = require("../server.js");
+console.log("sql");
 
-app.get('/',async (req,res)=>{
+
+router.get('/',async (req,res)=>{
     try{
-        const {data,error} = await sql.from("shoes").select();
+        const {data,error} = await sql.from('shoes').select();
         console.log(data);
         return res.send(data);
     }
     catch(error){
-        return res.send({error});
+        return res.send(error);
     }
 })
 
